7a88982ed3959db51496164fab5b9e2f39f3ccda
Sudhanshu Sharma <133215157+sudhanshubsr-dev@users.noreply.github.com>
2023-12-29 18:53:00 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 const BASE_URL = process.env.BASE_URL;
 
-app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+app.use(cors({credentials: true, origin: "https://pulseofme-frontend.vercel.app"}));
 app.use(express.urlencoded({extended: true}));
 
 app.use('/uploads',express.static(path.resolve('uploads')));
