5b099c45f301588712f526155d181072f9a857d9
MagnusDevelopr <sudhanshusharma103@gmail.com>
2023-12-30 00:51:51 +0530

    Fix API route paths

--- api/routes/index.router.js
+++ api/routes/index.router.js
@@ -4,16 +4,16 @@ import uploadFile from '../middlewares/multerS3.middleware.js';
 
 const router = express.Router();
 
-router.post('/api/register', userController.register);
-router.post('/api/login', userController.login);
-router.get('/api/profile', userController.profile);
-router.post('/api/logout', userController.logout);
-router.post('/api/createpost',uploadFile.single('file'), userController.createpost);
+router.post('api/register', userController.register);
+router.post('api/login', userController.login);
+router.get('api/profile', userController.profile);
+router.post('api/logout', userController.logout);
+router.post('api/createpost',uploadFile.single('file'), userController.createpost);
 
-router.get('/api/posts', userController.getposts);
-router.get('/api/post/:id', userController.getpostinfo);
-router.put('/api/post/:id', userController.updatepost);
-router.put('/api/updatepost', uploadFile.single('file'),userController.updatepost);
+router.get('api/posts', userController.getposts);
+router.get('api/post/:id', userController.getpostinfo);
+router.put('api/post/:id', userController.updatepost);
+router.put('api/updatepost', uploadFile.single('file'),userController.updatepost);
 export default router;
 
 
